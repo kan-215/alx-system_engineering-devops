42c9f78c656c9fcee5242172306c09db940fabe2
kan-215 <katamialvin369@gmail.com>
2024-06-30 21:38:05 +0300

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,40 +1,35 @@
-# making  changes to default ssh configu file using puppet
-# so that one can connect to a server without typing a password.
-rat
+# Configure SSH client settings using Puppet
+# Ensure connection to server without typing a password
+
 include stdlib
 
-file_line { 'SSH Private Key':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  replace            => true,
-  append_on_no_match => true
+# Define the SSH config file path
+$ssh_config_file = '/etc/ssh/ssh_config'
+
+# Ensure the IdentityFile is set to ~/.ssh/school
+file_line { 'Set SSH Private Key':
+  path    => $ssh_config_file,
+  line    => '    IdentityFile ~/.ssh/school',
+  match   => '^\s*IdentityFile\s+~/.ssh/.*$',
+  ensure  => present,
+  after   => '# Host-specific definitions',
 }
 
-# Regex match explanation
-#
-# ^       beginning of the line
-# [#]*  atleast one hash character
-# [\s]*  zero or more white space characters
-# (?i)IdentityFile case insensitive "IdentityFile"
-# [\s]+ at least one whitespace character
-# ~/.ssh/id_rsa The ssh private key file path we want to replace
-# $      end of the line
-
-file_line { 'Deny Password Auth':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  replace            => true,
-  append_on_no_match => true
+# Ensure PasswordAuthentication is disabled
+file_line { 'Disable Password Auth':
+  path    => $ssh_config_file,
+  line    => '    PasswordAuthentication no',
+  match   => '^\s*PasswordAuthentication\s+(yes|no)$',
+  ensure  => present,
+  after   => '# Host-specific definitions',
 }
 
-# Regex match explanation
+# Explanation of Regex Patterns:
 #
-# ^       beginning of the line
-# [#]*  atleast one hash character
-# [\s]*  zero or more white space characters
-# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
-# [\s]+ at least one whitespace character
-# (yes|no) with the value "yes" or the value "no"
-# $      end of the line
+# ^\s*                      - Matches the beginning of the line followed by any whitespace
+# IdentityFile\s+           - Matches the literal "IdentityFile" followed by at least one whitespace
+# ~/.ssh/.*$                - Matches any path starting with ~/.ssh/ and any characters until the end of the line
+#
+# ^\s*                      - Matches the beginning of the line followed by any whitespace
+# PasswordAuthentication\s+ - Matches the literal "PasswordAuthentication" followed by at least one whitespace
+# (yes|no)$                 - Matches either "yes" or "no" until the end of the line
