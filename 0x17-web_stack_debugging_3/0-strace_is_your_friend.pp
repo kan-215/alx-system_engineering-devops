0c720d5fb460d6c43b157b06d6d738ac278b2ef5
kan-215 <katamialvin369@gmail.com>
2024-08-14 22:14:19 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-# Fixes typing error in wp-settings.php
+# find out why Apache is returning a 500 error. 
+# Once the issue is found, fix it and then automate it using Puppet
+
 exec {'typing error':
   command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
