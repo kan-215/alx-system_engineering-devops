f52e47caaa6fac87ff22f6697e28d5c719189e23
kan-215 <katamialvin369@gmail.com>
2024-08-15 21:39:01 +0300

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    # Reddit API endpoint URL for getting subreddit information
-    # url = f"https://www.reddit.com/r/{subreddit}/about.json
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    # Custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'CustomUserAgent/1.0'}
-
-    # Make a GET request to the API
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the response is successful (status code 200)
-    if response.status_code == 200:
-        # Extract the number of subscribers from the JSON response
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        # Return 0 if the subreddit is invalid or the request failed
+    if response.status_code == 404:
         return 0
-
-
-subreddit = "python"
-print("Sub in r/{}: {}".format(subreddit, number_of_subscribers(subreddit)))
+    results = response.json().get("data")
+    return results.get("subscribers")
