7defdc46e460468dec2ec1af44a8e294951d0064
kan-215 <katamialvin369@gmail.com>
2024-08-22 22:19:43 +0300

    inginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,16 @@
-# fix request dropping in nginx
+# Puppet script to fix request dropping in Nginx by increasing ulimit and restarting Nginx
 
+# Increase the maximum number of file descriptors (ulimit) for Nginx
 exec { 'increase ulimit':
   path    => '/bin',
-  command => "sed -i 's/15/2000/g' /etc/default/nginx"
+  command => "sed -i 's/15/2000/g' /etc/default/nginx",
+  onlyif  => "grep -q 'ulimit -n 15' /etc/default/nginx",
+  notify  => Exec['nginx restart'],  # Triggers a restart after change
 }
 
+# Restart Nginx to apply changes
 exec { 'nginx restart':
   path    => '/usr/sbin',
-  command => 'service nginx restart'
+  command => 'service nginx restart',
+  refreshonly => true,  # Restart only when triggered
 }
