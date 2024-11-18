870a83a47249d4c9bb22971c14b0605ba32c0e2b
kan-215 <katamialvin369@gmail.com>
2024-11-18 11:42:09 +0300

    apllication server

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Lists the number of subs
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-
-    try:
-        # Make the request to Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # If the subreddit does not exist, return 0
-        if response.status_code != 200:
-            return 0
-
-        # Parse the JSON response
-        results = response.json().get("data")
-
-        # Return the number of subscribers, or 0 if not found
-        return results.get("subscribers", 0)
-
-    except (requests.RequestException, ValueError, AttributeError):
-        # Return 0 for any error in the request or parsing the JSON
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/aaorrico23)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
